22a375caf494b405178bf3fb907d4875a8efda55
LF236 <fernandorodriguez236@gmail.com>
2022-10-25 15:13:55 -0500

    .

--- code/models/socket.js
+++ code/models/socket.js
@@ -52,6 +52,7 @@ class Socket {
 
             // EVENTOS DE SICA 3
             socket.on( 'sica3-nuevo-ingreso', data => {
+                console.log( 'FERNANDO' );
                 this.io.emit( 'nuevo-ingreso', {
                     'tipo_ingreso': data.tipo_ingreso,
                     'id_cama' : data.id_cama,
